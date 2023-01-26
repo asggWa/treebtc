fb17b4b13dd52d7467418794bcea58d71de50c1a
asggWa <79282759+asggWa@users.noreply.github.com>
2023-01-26 18:21:39 +0100

    Create main.rs
    Propuesta 001

---
+++ main.rs
@@ -0,0 +1,19 @@
+fn main() {
+    println!("Desafío es:");
+    let mut _desafio=0;
+    println!("{}",_desafio);
+    _desafio=1;
+    println!("{}",_desafio);
+    _desafio=2;
+    println!("{}",_desafio);
+    _desafio=3;
+    _desafio=0;
+    println!("Baby sais{}",_desafio);
+    println!("Baby sais{}",_desafio);
+    println!("Father sais, burble, rr.Try:{}",_desafio);
+    _desafio=12340;
+    println!("Is, Q.Try:{}",_desafio);
+    _desafio=1230120100;
+    println!("Q.Try:{}",_desafio);
+    //_desafio=12340123401230120100;
+}
