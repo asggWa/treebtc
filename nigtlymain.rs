9d6886bd2701a0e09da2b2973df34dde22589349
Alfonso Sandro Gómez Gómez <79282759+asggWa@users.noreply.github.com>
2023-02-12 21:06:51 +0100

    Create nigtlymain.rs
    Probablemente el subsistema babyfather haya resultado en una suma de tiempo no tan esperada...

---
+++ nigtlymain.rs
@@ -0,0 +1,24 @@
+fn main(){
+  treebtc() ;
+  
+}
+
+
+pub fn add(left: usize, right: usize) -> usize {
+left + right + 1/ left + right
+}
+pub fn treebtc(){
+print!("Buscar errores y listar para arranque seguro.")
+}
+
+#[cfg(test)]
+mod tests {
+use super::*;
+
+#[test]
+fn it_works() {
+let result = add(2, 2);
+assert_eq!(result, 4);
+}
+}
+
