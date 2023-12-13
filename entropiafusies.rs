9147744a6b2db292826db48cfadb84941ebb0443
Alfonso Sandro Gómez Gómez <79282759+asggWa@users.noreply.github.com>
2023-12-13 06:18:23 +0100

    Create entropiafusies.rs
     el .tolm package.json .gitignore .env etc
    están bajo riesgo aquí.
    ESto es: de corregir el único warning, pudiera aparecer 32 errores y 472 warning. 
    No ponga esto en producción sin hablar con el equipo de soporte inicial. Si mi repositorio me ayuda y nos ayuda ... Que los españoles somos muy egocentricos, el fusionador de entropía sabe ser inclusivo a bajo nivel buscando la fortificación del tipado en c
    usando rust con web3 y testeando el dialing con php;
    happy hack¡¡¡
    Expect the chaos
    Catch the star 
    Arranca babilonia de lunares.

---
+++ entropiafusies.rs
@@ -0,0 +1,56 @@
+#[macro_use]
+extern crate static_assertions;
+use serde::{Deserialize, Serialize};
+#[derive(Serialize, Deserialize, Debug)]
+struct Point {
+    x: i32,
+    y: i32,
+}
+
+use proc_macro::TokenStream;
+use quote::quote;
+use syn::{parse_macro_input, DeriveInput};
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+sa::const_assert!(true); ///CUIDADO OVER DIAG
+let point = Point { x: 1, y: 2 };
+
+    // Convert the Point to a JSON string.
+    let serialized = serde_json::to_string(&point).unwrap();
+
+    // Prints serialized = {"x":1,"y":2}
+    println!("serialized = {}", serialized);
+
+    // Convert the JSON string back to a Point.
+    let deserialized: Point = serde_json::from_str(&serialized).unwrap();
+
+    // Prints deserialized = Point { x: 1, y: 2 }
+    println!("deserialized = {:?}", deserialized);
+let resp = reqwest::get("https://httpbin.org/ip")
+        .await?
+        .json::<HashMap<String, String>>()
+        .await?;
+    println!("{:#?}", resp);
+    Ok(())
+
+
+/*
+#[proc_macro_derive(MyMacro)]
+pub fn my_macro(input: TokenStream) -> TokenStream {
+    // Parse the input tokens into a syntax tree
+    let input = parse_macro_input!(input as DeriveInput);
+
+    // Build the output, possibly using quasi-quotation
+    let expanded = quote! {
+        // ...
+let mut a int;
+
+    };
+
+    // Hand the output tokens back to the compiler
+    TokenStream::from(expanded)
+}
+*/
+//Finaliza el main. Test prinln
+println!("Hola Web3");
+}
